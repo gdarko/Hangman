10d695e7616185d19add36a9793e87f94cbd7ce8
Korunoska Maja <majakorunoska@hotmail.com>
2015-05-07 18:47:49 +0200

    Formatting tweaks

--- Hangman/Body.cs
+++ Hangman/Body.cs
@@ -10,7 +10,6 @@ namespace Hangman
 {
     public class Body
     {
-        //private HangmanForm refmain;
         public Graphics g;
         
         public bool Head { get; set; }
@@ -21,50 +20,42 @@ namespace Hangman
         public bool RightLeg { get; set; }
 
         protected PictureBox pb { get; set; }
-
-
+		
         public Body(HangmanForm main, bool UseGraphics = true)
         {
             ResetBody();
             pb = main.pbGuyBox;
         }
-
         private void addHead()
         {
             drawImg(Hangman.Properties.Resources.head);
             Head = true;
         }
-
         private void addSpinal()
         {
             drawImg(Hangman.Properties.Resources.body);
             Spinal = true;
         }
-
         private void addLeftHand()
         {
             drawImg(Hangman.Properties.Resources.left_hand);
             LeftHand = true;
         }
-
         private void addRightHand()
         {
             drawImg(Hangman.Properties.Resources.right_hand);
             RightHand = true;
         }
-
         private void addLeftLeg()
         {
             drawImg(Hangman.Properties.Resources.left_leg);
             LeftLeg = true;
         }
-
         private void addRightLeg()
         {
             drawImg(Hangman.Properties.Resources.hanged);
             RightLeg = true;
         }
-
         private void ResetBody()
         {
             Head = false;
@@ -74,7 +65,6 @@ namespace Hangman
             LeftLeg = false;
             RightLeg = false;
         }
-
         public void Hang()
         {
             if (!Head)
@@ -93,7 +83,6 @@ namespace Hangman
             {
                 addRightHand();  
             }
-
             else if (!LeftLeg)
             {
                 addLeftLeg();
@@ -107,14 +96,9 @@ namespace Hangman
                 return;
             }
         }
-
         void drawImg(Bitmap Res)
         {
             pb.Image = new Bitmap(Res);
         }
-
-
     }
-
-    
 }
